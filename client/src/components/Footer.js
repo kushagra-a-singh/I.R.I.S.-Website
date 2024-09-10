12c6d226455fb07387e96155f3c7b96fd1997b59
purva-rana <purvarana1617@gmail.com>
2024-09-10 22:52:59 +0530

    Footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,31 +8,31 @@ function Footer() {
         <div className="container">
             <div className="row ">
               <div className="col-md-3 px-5">
-                <h5 class="py-2 px-5">About</h5>
-                <ul className="list-unstyled px-5">
+                <h5 class="py-2">About</h5>
+                <ul className="list-unstyled">
                   <li><Link to="/about" className="text-light">Club</Link></li>
                   <li><Link to="/about" className="text-light">Our Team</Link></li>
                 </ul>
               </div>
               <div className="col-md-3 px-5">
-                <h5 class="py-2 px-5">Events</h5>
-                <ul className="list-unstyled px-5">
+                <h5 class="py-2">Events</h5>
+                <ul className="list-unstyled">
                   <li><Link to="/events" className="text-light">Hackathons</Link></li>
                   <li><Link to="/events" className="text-light">Podcasts</Link></li>
                 </ul>
               </div>
               <div className="col-md-3 px-5">
-                <h5 class="py-2 px-5">Follow Us</h5>
-                <ul className="list-unstyled px-5">
+                <h5 class="py-2">Follow Us</h5>
+                <ul className="list-unstyled">
                   <li><a href="https://www.instagram.com/iris_mitwpu/" className="text-light" target="_blank" rel="noopener noreferrer">Instagram</a></li>
                   <li><a href="https://chat.whatsapp.com/Lnu3YpiEM4WDmwCjwDCY6n" className="text-light" target="_blank" rel="noopener noreferrer">WhatsApp Community</a></li>
                 </ul>
               </div>
               <div className="col-md-3 px-5">
-                <h5 class="py-2 px-5">Policies</h5>
-                <ul className="list-unstyled px-5">
+                <h5 class="py-2">Policies</h5>
+                <ul className="list-unstyled">
                 <li><Link to="/policy" className="text-white">Privacy Policy</Link></li>
-                <li><Link to="/terms" className="text-white">Terms and Conditions</Link></li>
+                <li><Link to="/terms" className="text-white">Terms & Conditions</Link></li>
                 <li><Link to="/refund" className="text-white">Refund Policy</Link></li>
                 </ul>
               </div>
